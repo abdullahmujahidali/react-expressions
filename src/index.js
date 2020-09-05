6744279916496f221c975f13ade2125d3e9d0213
Abdullah Mujahid <48271080+abdullahmujahidali@users.noreply.github.com>
2020-09-05 20:36:58 +0500

    inline style attached

--- src/index.js
+++ src/index.js
@@ -5,10 +5,20 @@ const fName = "Abdullah";
 const lName = "Mujahid";
 const img = "https://picsum.photos/200";
 
+const colorStyle = {
+  color: "red",
+  border: "2px solid black"
+};
+
 ReactDOM.render(
   <div>
     {/* //<h1>Hello {fName}+ " "+{lName} </h1> */}
-    <h1 className="heading" contentEditable="false" spellCheck="false">
+    <h1
+      className="heading"
+      style={colorStyle}
+      contentEditable="false"
+      spellCheck="false"
+    >
       Hello {`${fName} ${lName} `}!
     </h1>
 

4c7976f9fb4ac26f1622e2e747d4394c7a4d56b3
Abdullah Mujahid <48271080+abdullahmujahidali@users.noreply.github.com>
2020-09-05 20:15:20 +0500

    added picsum

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom";
 
 const fName = "Abdullah";
 const lName = "Mujahid";
-const luckyNum = 4;
+const img = "https://picsum.photos/200";
 
 ReactDOM.render(
   <div>
     {/* //<h1>Hello {fName}+ " "+{lName} </h1> */}
-    <h1> Hello {`${fName} ${lName} `}!</h1>
-    <p>Your lucky number is {luckyNum}</p>
+    <h1 className="heading" contentEditable="false" spellCheck="false">
+      Hello {`${fName} ${lName} `}!
+    </h1>
+
+    <div>
+      <img src={img} alt="pop" />
+    </div>
   </div>,
   document.getElementById("root")
 );
